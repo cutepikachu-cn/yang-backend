5bf095fe6901c2267c4ad9e6bf096b150f7ab8f5
2894462606 <2894462606@qq.com>
2024-04-20 19:54:17 +0800

    feat: 对象存储
    整合腾讯云对象存储SDK;
    对象存储管理开发;

---
+++ src/main/java/cn/cutepikachu/yangtuyunju/model/enums/FileUploadBizEnum.java
@@ -0,0 +1,61 @@
+package cn.cutepikachu.yangtuyunju.model.enums;
+
+import cn.hutool.core.util.ObjUtil;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author 笨蛋皮卡丘
+ * @version 1.0
+ */
+@Getter
+@AllArgsConstructor
+public enum FileUploadBizEnum {
+
+    USER_AVATAR("用户头像", "user_avatar"),
+    COMMODITY_IMG("商品图片", "commodity_img");
+
+    private final String text;
+
+    private final String value;
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<String> getValues() {
+        return Arrays.stream(values()).map(item -> item.value).collect(Collectors.toList());
+    }
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value
+     * @return
+     */
+    public static FileUploadBizEnum getEnumByValue(String value) {
+        if (ObjUtil.isEmpty(value)) {
+            return null;
+        }
+        for (FileUploadBizEnum anEnum : FileUploadBizEnum.values()) {
+            if (anEnum.value.equals(value)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
